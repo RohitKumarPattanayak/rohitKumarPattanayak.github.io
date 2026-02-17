f128da1361b7b9e84986dceec680b3746d7b796b
Rohit kumar <rohitkumarpattanayak@gmail.com>
2026-02-17 09:59:07 +0530

    P-08 db connection : db connnection and post API completed.

--- apps/api/app/main.py
+++ apps/api/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.routes import chat_route
 import uvicorn
 import os
+from app.core.database import engine, Base
 
 app = FastAPI(title="Rohit AI Portfolio API")
 
@@ -10,6 +11,12 @@ app.include_router(chat_route.router)
 @app.get("/")
 def root():
     return {"message": "AI Portfolio API Running"}
+
+@app.on_event("startup")
+async def startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
 
 if __name__ == "__main__":
     uvicorn.run(
