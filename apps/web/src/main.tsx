d8a1bd8070bdb58cdd242774a1dbddcc4987ced2
Rohit kumar <rohitkumarpattanayak@gmail.com>
2026-04-05 18:45:03 +0530

    P-76 performance : performance issue in reat ap needed react scan

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,8 +1,13 @@
+import { scan } from 'react-scan'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { AppProviders } from './app/providers.tsx'
 import { AppRouter } from './app/router.tsx'
+
+scan({
+  enabled: true
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
